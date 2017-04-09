c9c85f87416ad3e74896e626d98fba5b9c709f76
umutyalcin0723 <umutyalcin0723@hotmail.com>
2017-04-09 15:13:40 +0300

    ConditionStatement

---
+++ ConditionStatement/ConditionStatement/App.cs
@@ -0,0 +1,23 @@
+﻿namespace CSD
+{
+    class App
+    {
+        public static void Main()
+        {
+            int a, b ,c;
+
+            System.Console.Write("a: ");
+            a = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("b: ");
+            b = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("c: ");
+            c = int.Parse(System.Console.ReadLine());
+
+            int max = a > b ? a > c ? a : c : b > c ? b : c;
+
+            System.Console.WriteLine(max);
+        }
+    }
+}
